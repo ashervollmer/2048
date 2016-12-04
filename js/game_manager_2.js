f6b56d863d542a269d2c56fa4650ce8c98f28a1d
ashervollmer <begerreger@gmail.com>
2016-12-04 16:15:06 +0000

    2 functionality

--- js/game_manager_2.js
+++ js/game_manager_2.js
@@ -72,7 +72,7 @@ GameManager.prototype.addStartTiles = function () {
 // Adds a tile in a random position
 GameManager.prototype.addRandomTile = function () {
   if (this.grid.cellsAvailable()) {
-    var value = Math.random() < 0.9 ? 0 : 0;
+    var value = Math.random() < 0.9 ? 2 : 2 ;
     var tile = new Tile(this.grid.randomAvailableCell(), 0);
 
     this.grid.insertTile(tile);
@@ -171,7 +171,7 @@ GameManager.prototype.move = function (direction) {
           self.score += merged.value;
 
           // The mighty 2048 tile
-          if (merged.value === 2048) self.won = true;
+          if (merged.value === Goal) self.won = true;
         } else {
           self.moveTile(tile, positions.farthest);
         }
